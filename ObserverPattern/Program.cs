f8319f5054f0591e1cb0c6f66b27e1ee402c9346
Michiel van Rooy <michiel.vanrooy@entelect.co.za>
2023-07-25 15:54:53 +0200

    Observer Pattern example implemented

---
+++ ObserverPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using ObserverPattern;
+
+var subject = new Subject();
+var observerA = new ConcreteObserverA();
+subject.Attach(observerA);
+
+var observerB = new ConcreteObserverB();
+subject.Attach(observerB);
+
+subject.SomeBusinessLogic();
+subject.SomeBusinessLogic();
+
+subject.Detach(observerB);
+
+subject.SomeBusinessLogic();
+
+Console.ReadLine();
